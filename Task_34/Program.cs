2c15d071ce91bd76609c1b9062b4695e4c568c72
unknown <nastena-alhovik@yandex.ru>
2022-09-16 21:45:54 +0300

    add Task 34 Homework 5

---
+++ Task_34/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+
+int[] array = GetArray(20, 100, 999);
+Console.WriteLine(String.Join(",", array));
+EvenNumber(array);
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void EvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Количество четных чисел = " + count);
+}

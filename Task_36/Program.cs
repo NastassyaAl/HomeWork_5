3baa23b94e03d882bd1cc3f5dc6500c1a24434a0
unknown <nastena-alhovik@yandex.ru>
2022-09-17 18:32:39 +0300

    add Task 36 Homrwork 5

---
+++ Task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] array = GetArray(8, -30, 30);
+Console.WriteLine(String.Join(",", array));
+SummArray(array);
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void SummArray(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            summ+= array[i];
+        }
+    }
+    Console.WriteLine("summ = "+summ);
+}

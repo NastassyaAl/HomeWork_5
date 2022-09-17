89232a79b7b2023f03d207bbe2b0895fc4297004
unknown <nastena-alhovik@yandex.ru>
2022-09-17 22:14:11 +0300

    add Task 38 Homework 5

---
+++ Task_38/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+double[] array = GetArray(10, 0, 100);
+Console.WriteLine(String.Join(",", array));
+MaxMinArray(array);
+
+
+double[] GetArray(int size, int minvalue, int maxvalue)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minvalue, maxvalue);
+    }
+    return array;
+}
+
+void MaxMinArray(double[] array)
+{
+    
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= max)
+        {
+            max = array[i];
+        }
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] <= min)
+        {
+            min = array[i];
+        }
+    }
+    Console.WriteLine($"Разница между максимальным и минимальным значениями: {max-min}");
+}
